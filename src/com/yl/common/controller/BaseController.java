9f5d69423a0db16ee71191191416d88a867bad68
mokeily99 <mokeily99@126.com>
2019-04-28 08:22:17 +0800

    fd

--- src/com/yl/common/controller/BaseController.java
+++ src/com/yl/common/controller/BaseController.java
@@ -37,6 +37,7 @@ public class BaseController{
 		PrintWriter writer = null;
 		try {
 			writer = response.getWriter();
+			System.out.println("0000000");
 		} catch (IOException e) {
 			logger.error(e.getMessage(),e);
 		} finally {
