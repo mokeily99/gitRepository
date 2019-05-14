dd4161afc5b3bd930a226547132ad453cd98ff82
mokeily99 <mokeily99@126.com>
2019-05-14 14:38:58 +0800

    ss

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,19 +1,24 @@
 package com;
 
 import java.io.IOException;
-import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.session.SqlSession;
+
+import com.yl.common.util.DBUtil;
+import com.yl.transaction.seat.dao.SeatDao;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
 		System.out.println("========================");
 		
-		Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-		SqlSessionFactory factory = builder.build(reader);
-		System.out.println(factory);
+		SqlSession session = DBUtil.getDefaultSqlSession();
+		SeatDao mapper = session.getMapper(SeatDao.class);
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("seatID", "1000000002");
+		List<Map<String, String>> seatInfo = mapper.getSeatFreeBusyInfo(param);
+		System.out.println(seatInfo);
 	}
 }
